8fd276014b96fb92a433e317b91aab008fd6356f
Edward Ekelund <edwardd@axis.com>
2015-11-06 12:42:45 +0100

    diffeng: Fixed logging

--- diff/diffeng.py
+++ diff/diffeng.py
@@ -12,6 +12,22 @@ import logging
 import codecs
 
 
+def setup_logging(loglevel, logfile):
+    numeric_level = getattr(logging, loglevel.upper(), None)
+    if not isinstance(numeric_level, int):
+        raise ValueError('Invalid log level: %s' % loglevel)
+
+    logformat = "[%(asctime)s %(levelname)s] %(message)s"
+    dateformat = "%H:%M:%S"
+    if logfile:
+        logging.basicConfig(filename=logfile, format=logformat,
+                            level=loglevel, datefmt=dateformat)
+    else:
+        logging.basicConfig(format=logformat, level=loglevel,
+                            datefmt=dateformat)
+
+
+
 def main():
     """Main method to run Difference Engine™ standalone"""
     default_cutoff = 0
@@ -24,6 +40,8 @@ def main():
     parser.add_argument('-l', '--loglevel', help="Loglevel, valid values are: "
                         "DEBUG, INFO, WARNING, ERROR, CRITICAL",
                         default='WARNING')
+    parser.add_argument('--logfile', default="",
+                        help="Log to file instead of stdout.")
     parser.add_argument('-c', '--cutoff',
                         help="Cutoff level for correlations. Defaults to "
                         "{}".format(default_cutoff),
@@ -37,39 +55,33 @@ def main():
                         'are calculated from as well.')
     args = parser.parse_args()
 
+    # Deal with args
     diffdump = args.diffdump
     filename = args.filename
     output = args.output
     pretty = True if not args.minimized else False
 
-    # Set loglevel
-    numeric_level = getattr(logging, args.loglevel.upper(), None)
-    if not isinstance(numeric_level, int):
-        raise ValueError('Invalid log level: %s' % args.loglevel)
+    # Setup logging
+    setup_logging(args.loglevel, args.logfile)
 
-    logformat = "[%(levelname)s: %(message)s]"
-    logging.basicConfig(filename='difference_engine', format=logformat,
-                        level=args.loglevel)
-
+    logging.debug("Reading %s", filename)
     with codecs.open(filename, 'r', encoding="utf-8") as fileh:
         text = fileh.read()
 
+    logging.debug("Parsing json...")
     diff = parse_json(text)
 
+    logging.debug("Correlating...")
     correlation = correlate(diff)
 
-    # Append json extension if not provided
-    if not output.endswith(".json"):
-        output = output + ".json"
-
     with codecs.open(output, 'w', encoding='utf-8') as fileh:
-        print("Writing {}correlation to {}.".format(
-            "pretty " if pretty else "", output))
+        logging.info("Writing %scorrelation to %s.",
+                     "pretty " if pretty else "", output)
         fileh.write(json_dumps(correlation, pretty=pretty))
 
     if diffdump:  # Write diffdump if set
         with codecs.open(diffdump, 'w', encoding='utf-8') as fileh:
-            print("Writing diffdump to {}.".format(diffdump))
+            logging.info("Writing diffdump to %s.", diffdump)
             fileh.write(json_dumps(diff, pretty=pretty))
 
     if args.print_:
